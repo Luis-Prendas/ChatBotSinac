63bec15d22b87ec3a43fb555e39ae16433b52bb1
Daniel Prendas Chavarria <daniel.prendas@grupocma.com>
2022-09-29 11:32:16 -0600

    Now init bot chat auto

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -7,6 +7,31 @@ export default function ChatBot({ BOT_ID }) {
     const styleOptions = {
         hideUploadButton: true,
     };
+    
+    const store = window.WebChat.createStore(
+        {},
+        ({ dispatch }) => next => action => {
+            if (action.type === "DIRECT_LINE/CONNECT_FULFILLED") {
+               dispatch({
+                   meta: {
+                        method: "keyboard",
+                    },
+                    payload: {
+                        activity: {
+                              channelData: {
+                                   postBack: true,
+                              },
+                               //Web Chat will show the 'Greeting' System Topic message which has a trigger-phrase 'hello'
+                               name: 'startConversation',
+                               type: "event"
+                          },
+                     },
+                     type: "DIRECT_LINE/POST_ACTIVITY",
+                });
+          }
+          return next(action);
+       }
+    );
 
     useEffect(() => {
         console.log(BOT_ID)
@@ -18,6 +43,7 @@ export default function ChatBot({ BOT_ID }) {
                         directLine: window.WebChat.createDirectLine({
                             token: conversationInfo.token,
                         }),
+                        store,
                         styleOptions
                     },
                     webchat.current
